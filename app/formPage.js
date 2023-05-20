d25f0b30e610a42e03957429b2d69b84c1ed3b28
Mohammed Nasser <35014984+mohmdalfaha@users.noreply.github.com>
2023-05-20 16:22:11 +0300

    Update: setup routing & splashscreen

---
+++ app/formPage.js
@@ -0,0 +1,5 @@
+import { Text } from "react-native";
+
+export default function formPage() {
+  return <Text>Chat page</Text>;
+}
---
+++ app/index.js
@@ -0,0 +1,25 @@
+import { View } from "react-native";
+import { Link, Stack, useRouter, SplashScreen } from "expo-router";
+import { Button } from "react-native-paper";
+import { useEffect, useState } from "react";
+
+export default function Home() {
+    const [isReady, setReady] = useState(false);
+    const router = useRouter();
+
+    useEffect(() => {
+      // Perform some sort of async data or asset fetching.
+      setTimeout(() => {
+        setReady(true);
+      }, 1000);
+    }, []);
+
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      {/* Use the `Screen` component to configure the layout. */}
+      {!isReady && <SplashScreen />}
+      <Stack.Screen options={{ title: "Autism Ai" }} />
+      <Button onPress={() => router.push("formPage")}>START</Button>
+    </View>
+  );
+}
